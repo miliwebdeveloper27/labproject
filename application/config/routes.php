1ac32168050641e893fac983af9cf23c21d945d0
Milind <Milind>
2015-11-09 19:51:34 +0530

    change in origingal config

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,8 +38,8 @@
 |
 */
 
-$route['default_controller'] = "welcome";
-$route['404_override'] = '';
+/*$route['default_controller'] = "welcome";
+$route['404_override'] = '';*/
 
 $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'News/view/$1';
